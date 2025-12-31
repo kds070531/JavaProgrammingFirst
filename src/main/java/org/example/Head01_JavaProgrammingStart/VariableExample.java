57992a663c302385de476114887fff139ca0a586
김대성 <kds070532@gmail.com>
2025-12-31 18:46:08 +0900

    첫번쨰

---
+++ src/main/java/org/example/Head01_JavaProgrammingStart/VariableExample.java
@@ -0,0 +1,9 @@
+package org.example.Head01_JavaProgrammingStart;
+
+public class VariableExample {
+    public static void main(String[] args) {
+        int age = 30;
+        double temperature = 36.6;
+g
+    }
+}
